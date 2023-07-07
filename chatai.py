3b667e6b4c45ff35ff75bf350069608baec260f2
FriezerProgramist <egorganzin4@gmail.com>
2023-07-07 19:29:00 +0300

    add key and finish chatai

--- chatai.py
+++ chatai.py
@@ -1,6 +1,12 @@
+import os
 import openai
 
-openai.api_key = "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"
+from dotenv import load_dotenv as ld
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    ld(dotenv_path)
+
+openai.api_key = os.getenv("api_key")
 
 models = openai.Model.list()
 #print(models)
@@ -11,3 +17,9 @@ def handle_input(user_input):
         model="gpt-3.5-turbo", messages=[{"role": "user", "content": user_input}]
     )
     return copletion
+
+#print(handle_input(input()).choices[0].message.content)
+
+while True:
+    print(handle_input(input("You: ")).choices[0].message.content)
+
